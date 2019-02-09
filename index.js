3f6e0920ba829e8ec230a3620f3bab4b2386e01d
Carlos Sánchez Botello <carlos.sanchez.botello@gmail.com>
2019-02-08 20:52:50 -0600

    Calculadora con suma, resta, division y multiplicacion

---
+++ index.js
@@ -0,0 +1,24 @@
+const math = require('./math');
+
+console.log(math.suma(10, 20));
+console.log(math.resta(10, 20));
+console.log(math.multiplicacion(10, 20));
+console.log(math.division(0, 0));
+//if (true) {
+
+//} else {
+
+}
+
+
+//let nombre = "carsantello";
+//let edad = "36";
+//const cabello = "oscuro";
+
+//console.log('hola')
+//saludar();
+//function saludar(){
+//  console.log('hola' + nombre);
+//}
+
+// or cowsay.think()
